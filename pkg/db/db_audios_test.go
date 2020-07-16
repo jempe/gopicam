2b1e4a72033441eb0d0f654d933a2ff140de47cc
Juan Castro <info@jempe.org>
2020-07-15 19:44:11 -0500

    Update item unit test

--- pkg/db/db_audios_test.go
+++ pkg/db/db_audios_test.go
@@ -32,6 +32,8 @@ func TestAudioDb(t *testing.T) {
 
 	sampleAudio := Audio{FileType: "wav", Length: 200, Size: 213124, DeviceTime: 234234234}
 
+	sampleAudioUpdate := Audio{FileType: "aif", Length: 300, Size: 234343124, DeviceTime: 7457534234234}
+
 	tests = append(tests, audioTest{
 		name:    "Blank",
 		input:   Audio{},
@@ -156,6 +158,81 @@ func TestAudioDb(t *testing.T) {
 						t.Errorf("Updated was not saved correctly")
 					}
 				}
+			}
+
+			sampleAudioUpdate.ID = audioID
+
+			rowsAffected, err := database.UpdateAudio(sampleAudioUpdate, tt.fields)
+
+			if err != nil || rowsAffected != 1 {
+				if !tt.wantErr {
+					t.Errorf("Error saving item")
+				}
+			} else {
+				savedAudio, err := database.GetAudio(audioID)
+
+				if err != nil {
+					t.Errorf("Error getting item")
+				}
+
+				validID, idError := validator.UUID(savedAudio.ID)
+
+				if !validID || idError != nil {
+					t.Errorf("ID was not saved correctly")
+				}
+
+				if emptyOrContains(tt.fields, "FileType") {
+					if savedAudio.FileType != sampleAudioUpdate.FileType {
+						t.Errorf("FileType was not saved correctly")
+					}
+				} else {
+					if savedAudio.FileType != "" {
+						t.Errorf("FileType default value was not saved correctly")
+					}
+				}
+
+				if emptyOrContains(tt.fields, "Length") {
+					if savedAudio.Length != sampleAudioUpdate.Length {
+						t.Errorf("Length was not saved correctly")
+					}
+				} else {
+					if savedAudio.Length != 0 {
+						t.Errorf("Length default value was not saved correctly")
+					}
+				}
+
+				if emptyOrContains(tt.fields, "Size") {
+					if savedAudio.Size != sampleAudioUpdate.Size {
+						t.Errorf("Size was not saved correctly")
+					}
+				} else {
+					if savedAudio.Size != 0 {
+						t.Errorf("Size default value was not saved correctly")
+					}
+				}
+
+				if emptyOrContains(tt.fields, "DeviceTime") {
+					if savedAudio.DeviceTime != sampleAudioUpdate.DeviceTime {
+						t.Errorf("DeviceTime was not saved correctly")
+					}
+				} else {
+					if savedAudio.DeviceTime != 0 {
+						t.Errorf("DeviceTime default value was not saved correctly")
+					}
+				}
+
+				currentTime := time.Now()
+
+				if !savedAudio.Created.Before(savedAudio.Updated) {
+					t.Errorf("Created was not saved correctly")
+				}
+
+				if savedAudio.Updated != currentTime {
+					if savedAudio.Updated.After(currentTime) || savedAudio.Updated.Before(startTime) {
+						t.Errorf("Updated was not saved correctly")
+					}
+				}
+
 			}
 		})
 	}
